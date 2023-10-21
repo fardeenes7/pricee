3f2b4f783da28c5664f954f73b25e6a99ab1eea9
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-10-21 12:52:37 +0600

    commented everything in v1 admn to avoid being run

--- v1/admin.py
+++ v1/admin.py
@@ -1,56 +1,56 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import Product, Feature, Startech, Ryans, Techland, SubCategory, Category
+# from .models import Product, Feature, Startech, Ryans, Techland, SubCategory, Category
 
-from .tasks import refresh_records, clean_categories
-# Register your models here.
-
-
-
-class ProductAdmin(admin.ModelAdmin):
-    #list_display = ['title', 'status']
-    list_display = ['name', 'slug', 'sub_category','brand', 'model', 'best_price', 'startech', 'ryans', 'techland']
-    #ordering = ['title']
-    #change_list_template = 'product_change_list.html'
-    actions = [refresh_records]
+# from .tasks import refresh_records, clean_categories
+# # Register your models here.
 
 
-class TechlandAdmin(admin.ModelAdmin):
-    list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
-    ordering = ['last_updated']
 
-class StartechAdmin(admin.ModelAdmin):
-    list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
-    ordering = ['last_updated']
-
-class RyansAdmin(admin.ModelAdmin):
-    list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
-    ordering = ['last_updated']
+# class ProductAdmin(admin.ModelAdmin):
+#     #list_display = ['title', 'status']
+#     list_display = ['name', 'slug', 'sub_category','brand', 'model', 'best_price', 'startech', 'ryans', 'techland']
+#     #ordering = ['title']
+#     #change_list_template = 'product_change_list.html'
+#     actions = [refresh_records]
 
 
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    ordering = ['name']
-    actions = [clean_categories]
+# class TechlandAdmin(admin.ModelAdmin):
+#     list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
+#     ordering = ['last_updated']
+
+# class StartechAdmin(admin.ModelAdmin):
+#     list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
+#     ordering = ['last_updated']
+
+# class RyansAdmin(admin.ModelAdmin):
+#     list_display = ['link', 'price', 'regular_price', 'status', 'last_updated']
+#     ordering = ['last_updated']
 
 
-class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ['category', 'name', 'slug']
-    ordering = ['category']
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'slug']
+#     ordering = ['name']
+#     actions = [clean_categories]
+
+
+# class SubCategoryAdmin(admin.ModelAdmin):
+#     list_display = ['category', 'name', 'slug']
+#     ordering = ['category']
     
-"""
-admin.site.register(Product, ProductAdmin)
+# """
+# admin.site.register(Product, ProductAdmin)
 
-admin.site.register(Feature)
-admin.site.register(Startech, StartechAdmin)
-admin.site.register(Ryans, RyansAdmin)
-admin.site.register(Techland, TechlandAdmin)
-admin.site.register(SubCategory, SubCategoryAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Feature)
+# admin.site.register(Startech, StartechAdmin)
+# admin.site.register(Ryans, RyansAdmin)
+# admin.site.register(Techland, TechlandAdmin)
+# admin.site.register(SubCategory, SubCategoryAdmin)
+# admin.site.register(Category, CategoryAdmin)
 
-"""
+# """
 
 
-admin.site.site_header = "Pricee Admin"
-admin.site.site_title = "Pricee Admin Portal"
-admin.site.index_title = "Admin Area"
+# admin.site.site_header = "Pricee Admin"
+# admin.site.site_title = "Pricee Admin Portal"
+# admin.site.index_title = "Admin Area"
